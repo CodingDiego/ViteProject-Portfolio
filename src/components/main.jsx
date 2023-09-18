74f76c5c5adbd229d4104fc78ea1b7dba5b5d777
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-09-18 05:02:01 -0300

    Penultimate Commit: Add: Form Functionality and Backend

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,7 +6,7 @@ import { FaTwitter, FaFacebook, FaInstagram, FaLinkedinIn} from 'react-icons/fa'
 const Main = () => {
     return ( 
         <div id="main">
-            <img className='w-full h-screen object-cover object-left scale-x-[-1]' src={imagen} alt="" />
+            <img className='w-full h-screen object-cover object-left scale-x-[-1]' src={imagen} alt="/" />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
                 <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>{`I'm Diego Palacios`}</h1>
@@ -20,7 +20,7 @@ const Main = () => {
         'Tech Enthusiast',
         1000,
                 ]}
-                    wrapper="div"
+                    wrapper="span"
                      cursor={true}
                      style={{ fontSize: '1em', paddingLeft: '5px' }}
                     repeat={Infinity}
